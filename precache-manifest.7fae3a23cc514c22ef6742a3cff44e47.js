a39c97ec458aa48fd391f2162a1ccab3f65a28d0
Tomasz Gałuszka <tomasz.g@yggdrasilgaming.com>
2021-10-30 11:22:33 +0200

    initial

---
+++ precache-manifest.7fae3a23cc514c22ef6742a3cff44e47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48e4897ae02f85082c73d28088b8434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be3844822e271a3c3d1b",
+    "url": "/static/css/main.32632b94.chunk.css"
+  },
+  {
+    "revision": "18559f94c4311a42d70a",
+    "url": "/static/js/2.e07bafd1.chunk.js"
+  },
+  {
+    "revision": "be3844822e271a3c3d1b",
+    "url": "/static/js/main.89565e3d.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
